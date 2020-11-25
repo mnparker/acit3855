20c46048821fc7b36ab7b7c8899350afca33bc53
Mike Parker <michaelnparker@gmail.com>
2020-11-24 22:05:40 -0800

    added base_path to app api

--- audit_service/app.py
+++ audit_service/app.py
@@ -84,7 +84,7 @@ def get_env_reading(index):
 app = connexion.FlaskApp(__name__, specification_dir='')
 CORS(app.app)
 app.app.config['CORS_HEADERS'] = 'Content-Type'
-app.add_api("openapi.yml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yml", base_path="/audit_log", strict_validation=True, validate_responses=True)
 
 
 if __name__ == "__main__":
--- receiver_service/app.py
+++ receiver_service/app.py
@@ -83,7 +83,7 @@ def measure_environment(body):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 
 if __name__ == "__main__":
